98c7a1684bf030421df78e86555e42dc3f02ea1c
EmadNova <im.nova@yahoo.com>
2023-07-12 02:52:17 +0330

    1 text changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ export const Home = () => {
             <div className="landing__element">
                 <h1 className="element__title">Contact List</h1>
                 <span className="element__helper">Press the button down below to add contact</span>
-                <button className="element__btn" onClick={handleClick}>Add</button>
+                <button className="element__btn" onClick={handleClick}>Add Contact</button>
             </div>
             <div className="landing__contact">
                 <h1 className="contact__title">Your Contacts</h1>
